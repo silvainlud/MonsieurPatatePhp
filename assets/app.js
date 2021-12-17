b0cc741f13babfb54919710ed5b818d0d797e399
Ludwig SILVAIN <ludwig@silvain.eu>
2021-12-17 11:08:39 +0100

    feat : ajout react et PWA

--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,17 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
-
-// any CSS you import will output into a single css file (app.css in this case)
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './styles/app.css';
+import reportWebVitals from './reportWebVitals';
+import App from "./app/App";
 
-// start the Stimulus application
-import './bootstrap';
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
---
+++ src/Controller/IndexController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController
+{
+    #[Route("/", name: "index")]
+    public function Index() : Response
+    {
+        return $this->render("react.html.twig");
+    }
+}
