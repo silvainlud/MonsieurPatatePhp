54177d4b3fc95c1aa664c7beb8a110cf77fd3e33
Ludwig SILVAIN <ludwig@silvain.eu>
2021-12-18 21:10:33 +0100

    feat: service pour les url oauth discord

---
+++ src/Service/OauthDiscordUrl.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service;
+
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+class OauthDiscordUrl
+{
+    public function __construct(private string $clientId, private UrlGeneratorInterface $urlGenerator)
+    {
+    }
+
+    public function getUrlLogin(): string
+    {
+        $redirect = urlencode($this->urlGenerator->generate('login_discord_check', referenceType: UrlGeneratorInterface::ABSOLUTE_URL));
+
+        return "https://discord.com/api/oauth2/authorize?client_id={$this->clientId}&redirect_uri={$redirect}&response_type=code&scope=identify%20email";
+    }
+}
