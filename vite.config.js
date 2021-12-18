469ce22eda8de7d9a4036641dd2a381e4f630c7f
Ludwig SILVAIN <ludwig@silvain.eu>
2021-12-18 22:29:20 +0100

    feat: séparation des fichiers app/security

--- vite.config.js
+++ vite.config.js
@@ -21,7 +21,7 @@ export default defineConfig({
             },
             input: {
                 'app.js': './assets/app.js',
-                // 'security/app.js': './assets/app.js',
+                'security.js': './assets/security.js',
             }
         }
     }
