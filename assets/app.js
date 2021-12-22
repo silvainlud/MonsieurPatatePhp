4720d87137ddbc5454b844b79c3c1e8b14fae3b0
Ludwig SILVAIN <ludwig@silvain.eu>
2021-12-22 17:42:59 +0100

    feat: tom-select

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,14 @@
 import './styles/app.scss';
 import {toggleMenu} from "./components/responsive-menu";
-
+import TomSelect from "tom-select";
+import 'tom-select/src/scss/tom-select.scss'
 window.toggleMenu = toggleMenu;
+
+document.querySelectorAll("select.ts-select").forEach(x => {
+    let plugins = ['remove_button', 'dropdown_input']
+    if (x.multiple)
+        plugins.push("checkbox_options")
+    new TomSelect(x, {
+        plugins: plugins,
+    });
+})
