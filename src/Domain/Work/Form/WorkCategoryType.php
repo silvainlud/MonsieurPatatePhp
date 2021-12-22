2c057b9170accb330b391d22cfc839f8241847cc
Ludwig SILVAIN <ludwig@silvain.eu>
2021-12-22 20:59:56 +0100

    feat : prise en charge des catégories de devoirs

---
+++ src/Domain/Work/Form/WorkCategoryType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Domain\Work\Form;
+
+use App\Domain\Work\Entity\WorkCategory;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class WorkCategoryType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add("name", TextType::class, ["label" => "Nom de la catégorie", 'row_attr' => ["class" => "form-row"]])
+            ->add("active", CheckboxType::class, ["label" => "Activer la catégorie"])
+            ->add("submit", SubmitType::class, ["label" => "Sauvegarder"]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            "data_class" => WorkCategory::class,
+        ]);
+    }
+
+    public function getBlockPrefix(): string
+    {
+        return "work_category_form";
+    }
+}
--- src/Http/Main/Controller/ConfigurationController.php
+++ src/Http/Main/Controller/ConfigurationController.php
@@ -6,6 +6,8 @@ namespace App\Http\Main\Controller;
 
 use App\Domain\Guild\Entity\GuildSettings;
 use App\Domain\Guild\Form\GuildSettingsType;
+use App\Domain\Work\Entity\WorkCategory;
+use App\Domain\Work\Form\WorkCategoryType;
 use App\Infrastructure\Parameter\IParameterService;
 use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
@@ -18,13 +20,17 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/config/')]
 class ConfigurationController extends AbstractController
 {
+    public const QUERY_HIDDEN_CATEGORIES = "display_hidden_categories";
+
+
     public function __construct(
-        private IParameterService $parameterService,
+        private IParameterService      $parameterService,
         private EntityManagerInterface $em
-    ) {
+    )
+    {
     }
 
-    #[Route('work-channel', name: 'config_work_channel')]
+    #[Route('', name: 'config')]
     public function discordWorkChannel(Request $request): Response
     {
         $guildSettings = $this->em->getRepository(GuildSettings::class)->find($this->parameterService->getGuildId());
@@ -32,17 +38,47 @@ class ConfigurationController extends AbstractController
             $guildSettings = new GuildSettings($this->parameterService->getGuildId());
         }
 
+
+        if ((bool)$request->query->get(self::QUERY_HIDDEN_CATEGORIES, null) === true)
+            $categories = $this->em->getRepository(WorkCategory::class)->findBy([], ['name' => 'asc']);
+        else
+            $categories = $this->em->getRepository(WorkCategory::class)->findBy(["active" => true], ['name' => 'asc']);
+
         $form = $this->createForm(GuildSettingsType::class, $guildSettings);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             $this->em->persist($guildSettings);
             $this->em->flush();
 
-            return $this->redirectToRoute('config_work_channel');
+            return $this->redirectToRoute('config');
         }
 
         return $this->render('config/workChannel.html.twig', [
             'form' => $form->createView(),
+            "categories" => $categories,
         ]);
     }
+
+    #[Route("work-category/add", name: "work_category_add")]
+    #[Route("work-category/edit/{category}", name: "work_category_edit")]
+    public function modifyWorkCategory(Request $request, ?WorkCategory $category = null): Response
+    {
+
+        if ($category === null)
+            $category = new WorkCategory();
+        $form = $this->createForm(WorkCategoryType::class, $category);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->em->persist($category);
+            $this->em->flush();
+
+            return $this->redirectToRoute("config");
+        }
+
+        return $this->render("config/work_category/form.html.twig", [
+            "form" => $form->createView(),
+        ]);
+    }
+
 }
