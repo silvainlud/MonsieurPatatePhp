5144ccef8bc2ffc10f865aa5f114d5591dc36bdf
Ludwig SILVAIN <ludwig@silvain.eu>
2021-12-23 19:38:33 +0100

    feat : liste des devoirs futures

--- src/Http/Main/Controller/IndexController.php
+++ src/Http/Main/Controller/IndexController.php
@@ -4,16 +4,26 @@ declare(strict_types=1);
 
 namespace App\Http\Main\Controller;
 
+use App\Domain\Work\Entity\WorkCategory;
+use App\Domain\Work\Repository\WorkRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends AbstractController
 {
+
+    public function __construct(private WorkRepository $workRepository)
+    {
+    }
+
     #[Route('/', name: 'index')]
     public function Index(): Response
     {
-        return $this->render('base.html.twig');
+        $works = $this->workRepository->findCurrentWork();
+        return $this->render('index.html.twig', [
+            "works" => $works,
+        ]);
     }
 
     #[Route('/_ui', name: 'ui')]
