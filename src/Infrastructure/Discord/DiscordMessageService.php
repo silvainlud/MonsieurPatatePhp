3e3ad1da03b8b2c61c7670d0583849189809696a
Ludwig SILVAIN <ludwig@silvain.eu>
2021-12-25 15:57:29 +0100

    feat : edition d'un message discord

--- src/Infrastructure/Discord/DiscordMessageService.php
+++ src/Infrastructure/Discord/DiscordMessageService.php
@@ -14,20 +14,22 @@ class DiscordMessageService implements IDiscordMessageService
 {
     public function __construct(
         private HttpClientInterface $discordClient,
-    ) {
+    )
+    {
     }
 
     public function sendEmbeds(
-        string $channelId,
-        ?string $title = null,
-        ?string $description = null,
-        string $authorName = 'Monsieur Patate',
-        ?string $authorUrl = 'https://silvain.eu',
-        ?string $authorIconUrl = 'https://silvain.eu/favicon_256.png',
+        string    $channelId,
+        ?string   $title = null,
+        ?string   $description = null,
+        string    $authorName = 'Monsieur Patate',
+        ?string   $authorUrl = 'https://silvain.eu',
+        ?string   $authorIconUrl = 'https://silvain.eu/favicon_256.png',
         ?DateTime $timestamp = null,
-        ?string $footerText = null,
-        bool $retry = true
-    ): string|false {
+        ?string   $footerText = null,
+        bool      $retry = true
+    ): string|false
+    {
         return $this->send($channelId, [
             'embeds' => [
                 [
@@ -55,7 +57,7 @@ class DiscordMessageService implements IDiscordMessageService
             return $res->id;
         }
         if ($res->message === 'You are being rate limited.' && $retry && property_exists($res, 'retry_after')) {
-            sleep((int) round((int) $res->retry_after / 1000 + 1, mode: \PHP_ROUND_HALF_UP));
+            sleep((int)round((int)$res->retry_after / 1000 + 1, mode: \PHP_ROUND_HALF_UP));
 
             return $this->send($channelId, $options, false);
         }
@@ -68,5 +70,43 @@ class DiscordMessageService implements IDiscordMessageService
         $resp = $this->discordClient->request(Request::METHOD_GET, 'channels/' . $channelId . '/messages/' . $messageId);
 
         return $resp->getStatusCode() === Response::HTTP_OK;
+    }
+
+    public function editEmbeds(string $channelId, string $messageId, ?string $title = null, ?string $description = null, string $authorName = self::DEFAULT_AUTHOR_NAME, ?string $authorUrl = self::DEFAULT_AUTHOR_URL, ?string $authorIconUrl = self::DEFAULT_AUTHOR_ICON_URL, ?DateTime $timestamp = null, ?string $footerText = null, bool $retry = true): bool
+    {
+        return $this->edit($channelId, $messageId, [
+            'embeds' => [
+                [
+                    'title' => $title,
+                    'description' => $description,
+                    'timestamp' => $timestamp?->format(DateTimeInterface::W3C),
+                    'footer' => [
+                        'text' => $footerText,
+                    ],
+                    'author' => [
+                        'name' => $authorName,
+                        'url' => $authorUrl,
+                        'icon_url' => $authorIconUrl,
+                    ],
+                ],
+            ],
+        ], $retry);
+    }
+
+    public function edit(string $channelId, string $messageId, array $options, bool $retry = true): bool
+    {
+        $resp = $this->discordClient->request(Request::METHOD_PATCH, 'channels/' . $channelId . '/messages/' . $messageId, ['json' => $options]);
+
+        if ($resp->getStatusCode() === Response::HTTP_OK) {
+            return true;
+        }
+        $res = json_decode($resp->getContent(false));
+        if ($res->message === 'You are being rate limited.' && $retry && property_exists($res, 'retry_after')) {
+            sleep((int)round((int)$res->retry_after / 1000 + 1, mode: \PHP_ROUND_HALF_UP));
+
+            return $this->edit($channelId, $messageId, $options, false);
+        }
+
+        return false;
     }
 }
--- src/Infrastructure/Discord/IDiscordMessageService.php
+++ src/Infrastructure/Discord/IDiscordMessageService.php
@@ -8,19 +8,38 @@ use DateTime;
 
 interface IDiscordMessageService
 {
+    const DEFAULT_AUTHOR_NAME = 'Monsieur Patate';
+    const DEFAULT_AUTHOR_URL = 'https://silvain.eu';
+    const DEFAULT_AUTHOR_ICON_URL = 'https://silvain.eu/favicon_256.png';
+
     public function sendEmbeds(
         string $channelId,
         ?string $title = null,
         ?string $description = null,
-        string $authorName = 'Monsieur Patate',
-        ?string $authorUrl = 'https://silvain.eu',
-        ?string $authorIconUrl = 'https://silvain.eu/favicon_256.png',
+        string $authorName = self::DEFAULT_AUTHOR_NAME,
+        ?string $authorUrl = self::DEFAULT_AUTHOR_URL,
+        ?string $authorIconUrl = self::DEFAULT_AUTHOR_ICON_URL,
         ?DateTime $timestamp = null,
         ?string $footerText = null,
         bool $retry = true
     ): string|false;
 
+    public function editEmbeds(
+        string $channelId,
+        string $messageId,
+        ?string $title = null,
+        ?string $description = null,
+        string $authorName = self::DEFAULT_AUTHOR_NAME,
+        ?string $authorUrl = self::DEFAULT_AUTHOR_URL,
+        ?string $authorIconUrl = self::DEFAULT_AUTHOR_ICON_URL,
+        ?DateTime $timestamp = null,
+        ?string $footerText = null,
+        bool $retry = true
+    ): bool;
+
     public function send(string $channelId, array $options, bool $retry = true): string|false;
+
+    public function edit(string $channelId, string $messageId, array $options, bool $retry = true): bool;
 
     public function isMessageExist(string $channelId, string $messageId): bool;
 }
