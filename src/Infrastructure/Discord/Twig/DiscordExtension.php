0e16e51cb3a62050a313fd9a0ef2fbf8027f0afe
Ludwig SILVAIN <ludwig@silvain.eu>
2021-12-26 14:21:31 +0100

    feat : utilisation du nom d'utilisateur discord si possible

--- src/Infrastructure/Discord/Twig/DiscordExtension.php
+++ src/Infrastructure/Discord/Twig/DiscordExtension.php
@@ -9,20 +9,26 @@ use App\Infrastructure\Discord\DiscordUserService;
 use App\Infrastructure\Discord\Entity\DiscordGuild;
 use App\Infrastructure\Discord\Entity\DiscordRole;
 use App\Infrastructure\Discord\IDiscordGuildService;
+use App\Infrastructure\Parameter\IParameterService;
+use Symfony\Component\Security\Core\Security;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
 use Twig\TwigFunction;
 
 class DiscordExtension extends AbstractExtension
 {
-    public function __construct(private DiscordUserService $userService, private IDiscordGuildService $guildService)
+    public function __construct(private DiscordUserService   $userService,
+                                private IDiscordGuildService $guildService,
+                                private IParameterService $parameterService,private Security $security)
     {
+
     }
 
     public function getFunctions(): array
     {
         return [
             new TwigFunction('user_discord_avatar', [$this, 'getAvatarUser']),
+            new TwigFunction('user_discord_username', [$this, 'getDiscordUsername']),
             new TwigFunction('current_guild', [$this, 'getCurrentGuild']),
             new TwigFunction('current_guild_icon', [$this, 'getCurrentGuildIcon']),
             new TwigFunction('current_user_highest_role', [$this, 'getHighestRole']),
@@ -34,6 +40,17 @@ class DiscordExtension extends AbstractExtension
         return [
             new TwigFilter('discord_color', [$this, 'toDiscordColor']),
         ];
+    }
+
+    public function getDiscordUsername(): string
+    {
+        $u = $this->security->getUser();
+        if (!$u instanceof User)
+            return "???";
+        $discordUser = $this->guildService->getGuildMember($this->parameterService->getGuildId(), $u->getDiscordId());
+        if ($discordUser === null)
+            return $u->getUsername();
+        return $discordUser->getCompleteName();
     }
 
     public function getAvatarUser(User $user): string
