61ee428ec9a1780ef36132f62d058ef811549876
Ludwig SILVAIN <ludwig@silvain.eu>
2021-12-31 23:02:30 +0100

    fix(security) : mentions légales

---
+++ src/Http/Security/Controller/LegalController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Security\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/legal')]
+class LegalController extends AbstractController
+{
+    #[Route('/notice', name: 'legal_notice')]
+    public function legalNotice(): Response
+    {
+
+
+        return $this->render('security/legal/notice.html.twig', [
+
+        ]);
+    }
+}
